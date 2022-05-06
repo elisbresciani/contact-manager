465934ee09c7810f85da01bdbfff5b0665535362
Elis Bresciani <dddbresciani@gmail.com>
2022-05-06 14:35:38 -0300

    Create index.php

---
+++ wp-content/plugins/contact-management/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name:       Contact Manager
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0
+ * Author:            Elis Bresciani
+ * License:           GPL v2 or later
+ * Text Domain:       contact-management
+ */
