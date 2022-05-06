2f6d90de1f6a25e457492879f63e003e80e02e50
Elis Bresciani <dddbresciani@gmail.com>
2022-05-06 14:38:26 -0300

    Update index.php

--- wp-content/plugins/contact-management/index.php
+++ wp-content/plugins/contact-management/index.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Plugin Name:       Contact Manager
- * Description:       Handle the basics with this plugin.
- * Version:           1.0
- * Author:            Elis Bresciani
- * License:           GPL v2 or later
- * Text Domain:       contact-management
- */
+* Plugin Name: Contact Management Plugin
+* Description: A plugin to manage contacts
+* Version: 1.0.0
+* Author: Elis Bresciani
+* License: GPL2
+*/
